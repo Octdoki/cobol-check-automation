86d89753b574cddaf4e7f1b1afc1165d032eacd7
Doyeon Kim <aza98az@naver.com>
2025-10-30 15:30:16 +0900

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -5,7 +5,7 @@ LOWERCASE_USERNAME=$(echo " $ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists , create if it doesn 't
 if ! zowe zos - files list uss - files " /z/$LOWERCASE_USERNAME/cobolcheck"
-  & >/ dev / null; then
+  &>/ dev / null; then
   echo " Directory does not exist . Creating it ... "
   zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
 else
